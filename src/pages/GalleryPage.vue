52820d541a7d4a98cdc909b0efa34e6f8b08e920
Tristan Pinaudeau <tristan@tic.sh>
2021-05-06 19:57:22 +0200

    feat: import pic

--- src/pages/GalleryPage.vue
+++ src/pages/GalleryPage.vue
@@ -3,9 +3,11 @@
     <Header />
     <h1>Gallery for {{ slug }}</h1>
     <button v-on:click="displayImport = true">Importer une photo</button>
+    <!--<div v-for="pic in pics">{{ pic }}</div>-->
     <Modal v-if="displayImport" @close="displayImport = false">
-      <form>
-        <input type="file"/>
+      <form v-on:submit.prevent="importPic">
+        <!--<input @change="updateFiles" type="file" multiple />-->
+        <input @change="updateFile" type="file"/>
         <input type="submit"/>
       </form>
     </Modal>
@@ -23,12 +25,37 @@ export default {
     return {
       slug: '',
       displayImport: false,
+      picFile: undefined,
+      pics: [],
     }
   },
   mounted() {
+    this.user = JSON.parse(localStorage.getItem('user'));
     const params = new URLSearchParams(location.search);
     this.slug = params.get('slug')
     if (!this.slug) document.location = '/'
+  },
+  methods: {
+    updateFile(event) {
+      this.picFile = event.target.files[0];
+    },
+    importPic() {
+      this.picFile.arrayBuffer().then(
+        buffer => window.crypto.subtle.digest('SHA-1', buffer).then(
+          hashBuffer => {
+            const hashArray =  Array.from(new Uint8Array(hashBuffer));
+            const hash = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+            const fd = new FormData();
+            fd.append('file', this.picFile);
+            fetch(process.env.VUE_APP_BACKEND_URL + '/pic/' + hash, {
+              method: 'PUT',
+              headers: {'Authorization': 'Bearer ' + this.user.token},
+              body: fd
+            }).then(res => console.log(res))
+          }
+        )
+      );
+    }
   }
 }
 </script>
